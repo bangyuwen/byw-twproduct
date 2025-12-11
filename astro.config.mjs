d86e7bd5b9dd250b398d9c757667952119efc511
bangyuwen <bangyuwen@gmail.com>
2025-12-11 09:44:19 +0800

    refactor: rename project from byw-twproduct to byw-mit in package.json and astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://YOUR_USERNAME.github.io',
-  base: '/byw-twproduct',
+  base: '/byw-mit',
   output: 'static',
 });
